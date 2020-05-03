ddf9a924836890d7eb89cfd9af0f68d20bb7a0c9
Handras486 <hajduandras486@gmail.com>
2020-05-03 22:30:02 +0200

    Added MVC Webapp

---
+++ CSHARP/LoLesports/LoLesports.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LoLesports.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CSHARP/LoLesports/LoLesports.Web/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace LoLesports.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
